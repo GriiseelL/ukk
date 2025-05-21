caed8d9935b4a865f5fcf881e0a84a0074891569
Griselda <takun2971@gmail.com>
2025-05-21 15:12:35 +0700

    migrate

--- database/migrations/2025_05_15_125800_create_replay_coments_table.php
+++ database/migrations/2025_05_15_125800_create_replay_coments_table.php
@@ -11,11 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('friends', function (Blueprint $table) {
+        Schema::create('replay_coments', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
-            $table->foreignId('user_following')->constrained('users')->onDelete('cascade');
-            $table->string('status');
+            $table->foreignId('coments_id')->constrained('coments')->onDelete('cascade');
+            $table->text('content');
             $table->timestamps();
         });
     }
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('friends');
+        Schema::dropIfExists('replay_coments');
     }
 };
